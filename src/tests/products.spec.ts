88f71f587ae620bff7fe4ac1dbc13b92359dd7b6
Mohamed Gamal <mohamedgamalmohamed800@gmail.com>
2022-02-18 23:58:02 +0200

    add unit test for products model

---
+++ src/tests/products.spec.ts
@@ -0,0 +1,25 @@
+import ProductStore from '../models/product';
+
+
+
+const productStore = new ProductStore();
+
+describe('Product Model', () => {
+    it('test create new product', async () => {
+        const product = await productStore.create('blue toy', 400, 'toys');
+        expect(product).toEqual({
+            id: product.id,
+            name: 'blue toy',
+            price: 400,
+            category: 'toys'
+        });
+    });
+
+    it('test get single product', async () => {
+        const createdProduct = await productStore.create('green toy', 500, 'toys');
+        const productInDb = await productStore.show(String(createdProduct.id));
+        expect(productInDb).toEqual(createdProduct);
+    });
+
+
+});
